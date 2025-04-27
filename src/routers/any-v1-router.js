233c069798553b0fd0c2335441dc2b5afd450696
Niko Heller <acc.hellerniko@gmail.com>
2025-04-27 12:57:41 +0200

    any alive endpoint

--- src/routers/any-v1-router.js
+++ src/routers/any-v1-router.js
@@ -19,3 +19,18 @@ anyV1Router.get(
     res.send(roles)
   })
 )
+
+anyV1Router.get(
+  '/alive',
+  ash(async (req, res) => {
+    const now = new Date()
+    const pad = (num) => String(num).padStart(2, '0');
+    const formattedDate = `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())} ${pad(now.getHours())}:${pad(now.getMinutes())}:${pad(now.getSeconds())}`;
+
+    res.send({
+      "toType": "ServerAliveTO",
+      "alive": true,
+      "serverInfo": `Live-System (Version 1.5-74, Serverzeit ${formattedDate}, 1 active users)`
+    })
+  })
+)
